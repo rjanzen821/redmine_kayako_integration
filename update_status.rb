3366270416efc7e9e8b34e33ae9b6f502b2a5986
Randy Janzen <randy.janzen@gmail.com>
2013-02-26 16:41:20 -0600

    add comments

--- update_status.rb
+++ update_status.rb
@@ -22,25 +22,20 @@ end
 #    config.logger = Logger.new(STDOUT)
 #end
 
-#Get kayako custom field contents, parse redmine ids, get current redmine status
-#post updated kay custom field
+#Get kayako custom field contents
 tickets = KayakoClient::Ticket.all(3)
 tickets.each do |ticket|
 	fields = KayakoClient::TicketCustomField.get(ticket.id)
   postString = ""
-  red_idAray = fields['2azuubvzms0b'].scan(/\b\d{1}\b/)
+  red_idAray = fields['2azuubvzms0b'].scan(/\b\d{1}\b/) #parse redmine ids
     red_idAray.each do |red_id|
        if Issue.exists?(red_id) == true
-       red_status = Issue.find(red_id).status.name
-       postString += "##{red_id}|#{red_status}|   "
+       red_status = Issue.find(red_id).status.name #get current redmine status
+       postString += "##{red_id}|#{red_status}|   " #rebuild kayako custom field string
        else
-        postString += "##{red_id}|NA|   "
+        postString += "##{red_id}|NA|   " #handle non-existent redmine issues
         end
       end
-  fields['2azuubvzms0b'] = postString
+  fields['2azuubvzms0b'] = postString #post updated kayako custom field string
   fields.post
 end
-
-#$_SWIFT_TicketObject->UpdatePool('lastactivity', DATENOW);
-##update recent update time in kayako, every 3 mins?? only ofter red update...
-##Kayako tickets reference MANY redmine issues from 'Public' project and 'Private'
