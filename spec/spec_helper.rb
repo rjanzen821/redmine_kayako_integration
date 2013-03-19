8584298bdc5639773347e934208ce21517dd40be
randy <randy.janzen@gmail.com>
2013-03-19 17:12:09 -0500

    2 specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require File.expand_path(File.dirname(__FILE__) + '/../update_status')
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
--- update_status.rb
+++ update_status.rb
@@ -5,36 +5,47 @@ require "kayako_client"
 require "active_resource"
 require "logger"
 
-# Kayako setup
-kayako_department = 3
-kayako_issue_field_identifier = '2azuubvzms0b'
-KayakoClient::Base.configure do |config| # Kayako API Configuration uses kayako_client gem
-  config.api_url    = # kayako_url
-  config.api_key    = # kayako_key
-  config.secret_key = # kayako_secret
-end
-
-# Redmine setup
 class RedmineIssue < ActiveResource::Base # Redmine API configuration and Issue model
-  # redmine info here
-  self.site     = # redmine_url
-  self.user     = # redmine_user
-  self.password = # redmine_password
-  self.format   = :xml
 end
+class RedmineKayako
+  attr_accessor :kayako_department
+  attr_accessor :kayako_issue_field_identifier
 
-# Logger
-KayakoClient::Base.configure do |config| #logger config
-  config.logger = Logger.new(STDOUT)
-end
+  def setup
+    # Kayako setup
+    self.kayako_department = 3
+    self.kayako_issue_field_identifier = '2azuubvzms0b'
+    KayakoClient::Base.configure do |config| # Kayako API Configuration uses kayako_client gem
+      config.api_url    = ""# kayako_url
+      config.api_key    = ""# kayako_key
+      config.secret_key = ""# kayako_secret
+    end
 
-KayakoClient::Ticket.all(kayako_department).each do |ticket|
-	# Get kayako custom field contents
-  kayako_custom_fields = KayakoClient::TicketCustomField.get(ticket.id)
-  # extract redmine ids
-  redmine_issue_ids = kayako_custom_fields[kayako_issue_field_identifier].scan(/\#(\d+)/).flatten.map(&:to_i)
-  # assemble a string of redmine ids and statuses, e.g. "#123|open|  #957|closed|"
-  kayako_custom_fields[kayako_issue_field_identifier] = redmine_issue_ids.map{|id| RedmineIssue.exists?(id) ? "##{id}|#{RedmineIssue.find(id).status.name}|" : "##{id}|NA|" }.join("  ")
-  # post updated kayako custom field string
-  kayako_custom_fields.post
+    # Redmine setup
+    #class RedmineIssue < ActiveResource::Base # Redmine API configuration and Issue model
+      # redmine info here
+      RedmineIssue.site     = ""# redmine_url
+      RedmineIssue.user     = ""# redmine_user
+      RedmineIssue.password = ""# redmine_password
+      RedmineIssue.format   = :xml
+    #end
+
+    # Logger
+    KayakoClient::Base.configure do |config| #logger config
+      config.logger = Logger.new(STDOUT)
+    end
+  end
+
+  def update_kayako
+    KayakoClient::Ticket.all(self.kayako_department).each do |ticket|
+    	# Get kayako custom field contents
+      kayako_custom_fields = KayakoClient::TicketCustomField.get(ticket.id)
+      # extract redmine ids
+      redmine_issue_ids = kayako_custom_fields[self.kayako_issue_field_identifier].scan(/\#(\d+)/).flatten.map(&:to_i)
+      # assemble a string of redmine ids and statuses, e.g. "#123|open|  #957|closed|"
+      kayako_custom_fields[self.kayako_issue_field_identifier] = redmine_issue_ids.map{|id| RedmineIssue.exists?(id) ? "##{id}|#{RedmineIssue.find(id).status.name}|" : "##{id}|NA|" }.join("  ")
+      # post updated kayako custom field string
+      kayako_custom_fields.post
+    end
+  end
 end
